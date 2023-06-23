9928375f61afbcfd04c17bcb30f7c587d93f8803
Alejandro1103 <alejaf2211@gmail.com>
2023-06-22 20:32:07 -0600

    Commit Semana 6

--- tiendaa/src/main/java/com/Tienda/service/IProductService.java
+++ tiendaa/src/main/java/com/Tienda/service/IProductService.java
@@ -3,4 +3,6 @@ import com.Tienda.entities.Product;
 import java.util.List;
 public interface IProductService {
     List<Product> getAllProducts();
+      void save(Product product);
+      void delete(Product product);
 }
